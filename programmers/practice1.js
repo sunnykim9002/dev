8b8f63b26637d871880dacc0d6df971ce3eb520f
minsun kim <tlsrlqkdrl91@naver.com>
2023-05-24 00:51:56 +0900

    프로그래머스 추가

--- programmers/practice1.js
+++ programmers/practice1.js
@@ -4,3 +4,25 @@ function solution(num1, num2) {
     var answer = num1 === num2 ? 1 : -1;
     return answer;
 }
+
+// 두수의 나눗셈
+function solution(num1, num2) {
+    return parseInt(num1/num2*1000);
+}
+
+// 분수의 덧셈
+function solution(denum1, num1, denum2, num2) {
+    // 분자
+    let topNum = num1*denum2 + num2*denum1
+    // 분모
+    let botNum = num1*num2
+    // 최대 공약수
+    let maximum = 1
+    // 약분
+    for(let i = 1 ; i <= topNum ; i ++) {
+        if(topNum%i === 0 && botNum%i === 0) {
+            maximum = i
+        }
+    }
+    return [topNum/maximum, botNum/maximum]
+}
