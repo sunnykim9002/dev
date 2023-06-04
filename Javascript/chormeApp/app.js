e66dd2292506d6be7216c391f9aeb04322b7695e
sunnykim9002 <122718243+sunnykim9002@users.noreply.github.com>
2023-06-05 01:00:05 +0900

    js 커밋

--- Javascript/chormeApp/app.js
+++ Javascript/chormeApp/app.js
@@ -1,15 +1,26 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
 
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username"
 
-function handleLinkClick(e) {
-    e.preventDefault();
-    alert("click")
-}
-function onLoginSubmit(e) {
-    e.preventDefault();
-    loginForm.classList.add("hidden");
+function onLoginSubmit() {
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username)
+    greeting.innerText = `Hello ${username} keep going`;
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+
+if (savedUsername == null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit",onLoginSubmit )
+} else {
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+
+}
