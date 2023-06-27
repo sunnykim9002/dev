89764d780cd9859cb22b3ed8d7683b709fa5efa4
sunnykim9002 <122718243+sunnykim9002@users.noreply.github.com>
2023-06-27 23:50:06 +0900

    js 업데이트

--- programmers/practice7.js
+++ programmers/practice7.js
@@ -4,4 +4,20 @@ function solution(strlist) {
     return strlist.map((x)=> x.length);
 }
 
-// 
+// 머쓱이보다 키 큰 사람
+function solution(array, height) {
+    return array.filter((x)=> x>height).length
+    }
+
+
+// 순서쌍의 개수
+
+    function solution(n) {
+        let answer = "0";
+        for( i = 0; i<=n; i ++) {
+            if( n % i == 0) {
+                answer ++
+            }
+        }
+        return answer
+    }
