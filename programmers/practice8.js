4d933bef8946ee84642f652f21619295025fc2be
sunnykim9002 <122718243+sunnykim9002@users.noreply.github.com>
2023-06-28 13:26:50 +0900

    js 업데이트

--- programmers/practice8.js
+++ programmers/practice8.js
@@ -5,3 +5,15 @@ function solution(numbers, num1, num2) {
 }
 
 // 옷가게 할인 받기
+function solution(price) {
+    if(price >= 500000) {
+        price *= 0.8;
+    } else if(price >= 300000) {
+        price *= 0.9;
+    } else if(price >= 100000) {
+        price *= 0.95;
+    }
+    return Math.floor(price);
+}
+
+// 자릿수 더하기
