6124ebd801341f5a17f5d9fb715be08a52e1d37a
sunnykim9002 <122718243+sunnykim9002@users.noreply.github.com>
2023-08-01 23:24:09 +0900

    js 업데이트

--- programmers/practice12.js
+++ programmers/practice12.js
@@ -10,3 +10,14 @@ function solution(rsp) {
         }
     } return result;
 }
+
+
+function solution(rsp) {
+    let arr = {
+        2: 0,
+        0: 5,
+        5: 2
+    };
+    var answer = [...rsp].map(v => arr[v]).join("");
+    return answer;
+}
