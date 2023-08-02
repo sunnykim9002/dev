01d59ac56039317a0ce8df8520274e2a9e34a22b
sunnykim9002 <122718243+sunnykim9002@users.noreply.github.com>
2023-08-02 14:23:11 +0900

    js 업데이트

--- programmers/practice13.js
+++ programmers/practice13.js
@@ -4,3 +4,16 @@ function solution(i, j, k) {
     for(i; i<=j; i++){a = a + i}
 	return a
 }
+
+// 개미 군단
+function solution(hp) {
+    // 5 3 1
+    //장군개미 수
+   let x = Math.floor(hp / 5)
+    //병정개미 수 
+   let y = Math.floor((hp - (x*5)) / 3)
+   // 일개미 수
+   let z = hp - (x*5) - (y*3)
+   
+   return x + y + z 
+}
