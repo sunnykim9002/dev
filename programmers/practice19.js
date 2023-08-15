c14235eef78c478a66a76a88a54aac6de05ad160
sunnykim9002 <122718243+sunnykim9002@users.noreply.github.com>
2023-08-15 23:20:00 +0900

    js 업데이트

--- programmers/practice19.js
+++ programmers/practice19.js
@@ -16,3 +16,37 @@ function solution(my_string, k) {
 function solution(my_string, k) {
     return my_string.repeat(k)
 }
+
+
+
+// 수 조작하기 
+function solution(n, control) {
+    let array = control.split("")
+    let answer = n;
+      
+      
+      for ( let a of array) {
+          if( a == "w")
+          {answer = answer +1;}
+  
+      
+      if ( a== "s"){
+          answer = answer -1;}
+       
+      
+      if ( a == "d") {answer = answer + 10} 
+      if ( a == "a") { answer = answer - 10}
+      }
+      return answer 
+    
+  }
+
+// 다른 사람의 풀이 
+// map 안에서 함수 쓰기
+
+function solution(n, control) {
+    return [...control].map(x => setValue(x)).reduce((a, b) => a+b, n);
+}
+function setValue(s){
+    return s==="w" ? 1 : s==="s" ? -1 : s==="d" ? 10 : -10;
+}
