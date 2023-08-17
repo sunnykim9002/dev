a84240ed38d10ee3b56861af3827356116098153
sunnykim9002 <122718243+sunnykim9002@users.noreply.github.com>
2023-08-17 23:54:53 +0900

    js 업데이트

--- programmers/practice20.js
+++ programmers/practice20.js
@@ -7,4 +7,23 @@ function solution(num_list, n) {
     return 0
 }
 
-//
+// 문자열 정렬하기(1)
+function solution(my_string) {
+    
+    let array = [...my_string]
+    let numArray = [];
+    
+    for(let a of array) {
+        if (Number(a) == a ) numArray.push(+a) 
+    }
+    
+    return numArray.sort((a,b)=> a-b)
+    
+}
+
+// 문자열 정렬하기(1) 다른사람의 풀이
+// 참고 - 정규식 사용
+// 참고 - map 함수에 v 붙이기
+function solution(my_string) {
+    return my_string.replace(/[^\d]/g,'').split('').map(v=>+v).sort();
+}
